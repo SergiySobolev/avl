8c8877a474dd46059219df252516788524962d1f
SergiySobolev <sergiy.sobolev@gmail.com>
2016-09-22 13:17:59 +0300

    Improve fromArray creation with opportunity to provide null elements

--- src/main/java/com/sbk/avl/AvlTreeUtils.java
+++ src/main/java/com/sbk/avl/AvlTreeUtils.java
@@ -1,17 +1,19 @@
 package com.sbk.avl;
 
+import java.util.Objects;
+
 public class AvlTreeUtils {
-    static AvlTree fromArray(int... a) {
+    static AvlTree fromArray(Integer... a) {
         if(a.length == 0) return new AvlTree(null);
         TreeNode treeNode = new TreeNode(a[0]);
         fromArray(treeNode, 0, a);
         return new AvlTree(treeNode);
     }
 
-    private static void fromArray(TreeNode root, int startIndex, int... a) {
+    private static void fromArray(TreeNode root, int startIndex, Integer... a) {
         int leftSubtreeIdx = 2*startIndex + 1;
         int rightSubtreeIdx = 2*startIndex + 2;
-        if(leftSubtreeIdx < a.length) {
+        if(leftSubtreeIdx < a.length && Objects.nonNull(a[leftSubtreeIdx])) {
             TreeNode left = TreeNode.builder()
                     .val(a[leftSubtreeIdx])
                     .parent(root)
@@ -19,7 +21,7 @@ public class AvlTreeUtils {
             root.setLeft(left);
             fromArray(left, leftSubtreeIdx, a);
         }
-        if(rightSubtreeIdx < a.length) {
+        if(rightSubtreeIdx < a.length && Objects.nonNull(a[rightSubtreeIdx])) {
             TreeNode right = TreeNode.builder()
                     .val(a[rightSubtreeIdx])
                     .parent(root)
--- src/test/java/com/sbk/avl/AvlTreeUtilsTest.java
+++ src/test/java/com/sbk/avl/AvlTreeUtilsTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 
 public class AvlTreeUtilsTest {
     @Test
-    public void testFromArray() throws Exception {
+    public void test_fromArray_1() throws Exception {
         AvlTree tree = fromArray(1,2,3);
         TreeNode tn = tree.getRoot();
         assertEquals(tn.getVal(), 1);
@@ -16,6 +16,36 @@ public class AvlTreeUtilsTest {
         assertEquals(tn.getLeft().getParent(), tn);
         assertEquals(tn.getRight().getVal(),3);
         assertEquals(tn.getRight().getParent(),tn);
+    }
+
+    @Test
+    public void test_fromArray_2() throws Exception {
+        AvlTree tree = fromArray(1,2,3,null,5);
+        TreeNode tn = tree.getRoot();
+        assertEquals(tn.getVal(), 1);
+        assertNull(tn.getParent());
+        assertEquals(tn.getLeft().getVal(), 2);
+        assertEquals(tn.getLeft().getParent(), tn);
+        assertEquals(tn.getRight().getVal(),3);
+        assertEquals(tn.getRight().getParent(),tn);
+        assertNull(tn.getLeft().getLeft());
+        assertEquals(tn.getLeft().getRight().getVal(), 5);
+    }
+
+    @Test
+    public void test_fromArray_3() throws Exception {
+        AvlTree tree = fromArray(1,2,3,null,5,6);
+        TreeNode tn = tree.getRoot();
+        assertEquals(tn.getVal(), 1);
+        assertNull(tn.getParent());
+        assertEquals(tn.getLeft().getVal(), 2);
+        assertEquals(tn.getLeft().getParent(), tn);
+        assertEquals(tn.getRight().getVal(),3);
+        assertEquals(tn.getRight().getParent(),tn);
+        assertNull(tn.getLeft().getLeft());
+        assertEquals(tn.getLeft().getRight().getVal(), 5);
+        assertEquals(tn.getRight().getLeft().getVal(), 6);
+        assertEquals(tn.getRight().getLeft().getParent(), tn.getRight());
     }
 
 }
